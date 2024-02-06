8dbfe3b054542d5d5c15401a2e53fd097d141bb5
Daniel Olofsson <danielbarillo@gmail.com>
2024-02-06 22:05:04 +0100

    directorize fool

---
+++ elva-website/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "elvaweb": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3elvawebstorage8c3617e6": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
