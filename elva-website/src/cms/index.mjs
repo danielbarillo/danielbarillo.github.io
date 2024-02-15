afaad7d8e9d12d88688c84208fa51937dfa3c14f
Daniel Olofsson <danielbarillo@gmail.com>
2024-02-15 21:57:56 +0100

    removed old shit

--- elva-website/src/cms/index.mjs
+++
@@ -1,6 +0,0 @@
-import { settings } from "./settings.mjs";
-import { page } from "./page.mjs";
-import { post } from "./post.mjs";
-import { project } from "./project.mjs";
-
-export { settings, page, post, project };
